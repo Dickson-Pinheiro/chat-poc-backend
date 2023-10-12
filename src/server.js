18d87e4b7ae26dea6aec7264d22047cbd4599330
Dickson-Pinheiro <dicksonpinheiro4@gmail.com>
2023-10-12 19:39:41 -0300

    fix: remove headers congig

--- src/server.js
+++ src/server.js
@@ -25,15 +25,6 @@ io.on("connection", (socket) => {
 app.get("/", (req, res) => {
     res.send("Giphy Chat Server is running successfully");
 });
-  
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-})
-
 
 httpServer.listen(8080, () => {
     console.log('Server is running')
